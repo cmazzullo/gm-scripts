bd5912d9359d76f601693f4bdf59e3a67874fc11
cmazzullo <chris.mazzullo@gmail.com>
2017-02-07 22:48:03 -0500

    First commit

---
+++ cleanfacebook.user.js
@@ -0,0 +1,108 @@
+// ==UserScript==
+// @name        cleanfacebook
+// @namespace   tag:chris.mazzullo@gmail.com,2015-07:ChrisMazzullo
+// @description Remove sidebars and sponsored posts
+// @include     http://www.facebook.com*
+// @include     https://www.facebook.com*
+// @version     1
+// @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+
+$(document).ready(function () { // get rid of unnecessary bars
+    $(rightCol).hide();
+    $(leftCol).hide();
+    $(findFriendsNav).hide();
+});
+
+function removeAds (jNode) {
+    if (/Sponsored/i.test (jNode.text())) {
+        //jNode.hide();
+        jNode.empty();
+        jNode.css('background', '#eee');
+        jNode.append('<div style="text-align: center;">~clean facebook~</div>');
+    }
+}
+
+waitForKeyElements ('div[role="article"]', removeAds);
+
+// Credit: http://stackoverflow.com/a/8283815
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+function waitForKeyElements ( 
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
